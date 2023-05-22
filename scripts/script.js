b4e7220f33ba3521b576b80a76792c5d2cccd898
reinaldogpn <reinaldogpn@outlook.com>
2023-05-22 12:27:32 -0300

    ...

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+function enviarFormulario(form) {
+    var dadosFormulario = new FormData(form);
+    var action = form.querySelector('[name="action"]').value;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'authentication.php', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Lógica para manipular a resposta do servidor, se necessário
+            var response = JSON.parse(xhr.responseText);
+            if (response.error) {
+                alert(response.message);
+            } else {
+                // Sucesso no login ou registro
+                alert(response.message);
+                window.location.href = "pagina-inicial.php";
+            }
+        }
+    };
+    xhr.send(dadosFormulario);
+}
