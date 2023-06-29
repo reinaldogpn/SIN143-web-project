711d7e6ea1a4697d8573ca9c8062a8117fb846df
reinaldogpn <reinaldogpn@outlook.com>
2023-06-29 18:46:41 -0300

    v2.0

--- sql/create.sql
+++ sql/create.sql
@@ -26,7 +26,7 @@ CREATE TABLE `events` (
     `location` VARCHAR(255) NOT NULL,
     `category` VARCHAR(100) NOT NULL,
     `price` DECIMAL(10, 2),
-    `image` VARCHAR(255),
+    `image` TEXT,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -70,39 +70,4 @@ ALTER TABLE `reviews`
 ALTER TABLE `reviews`
     ADD FOREIGN KEY (`event_id`) REFERENCES `events`(`id`);
 
--- INSERÇÕES --
-
-INSERT INTO `roles` (`role_name`) VALUES ('organizador');
-
-INSERT INTO `roles` (`role_name`) VALUES ('participante');
-
-INSERT INTO `roles` (`role_name`) VALUES ('administrador');
-
-INSERT INTO `categories` (`category_name`) VALUES ('festas');
-
-INSERT INTO `categories` (`category_name`) VALUES ('bares');
-
-INSERT INTO `categories` (`category_name`) VALUES ('shows');
-
-INSERT INTO `categories` (`category_name`) VALUES ('música ao vivo');
-
-INSERT INTO `categories` (`category_name`) VALUES ('teatros');
-
-INSERT INTO `categories` (`category_name`) VALUES ('cursos');
-
-INSERT INTO `categories` (`category_name`) VALUES ('feiras');
-
-INSERT INTO `users` (`name`, `email`, `password`, `role`) VALUES ('Administrador', 'admin@ufv.br', '$2y$10$U3vDQKKEdC2BFUXzX4K6iupFqNDAoNpW/QwI/y5QhmFUuWk3xLc.W', 'administrador');
-
-INSERT INTO `events` (`title`, `description`, `date`, `time`, `location`, `category`, `price`, `image`) VALUES ('Título Exemplo #1', 'Descrição exemplo.', '2023-06-20', '19:00:00', 'Rio Paranaíba, MG', 'festas', 200.00, 'img_path/img_01.png');
-
-INSERT INTO `events` (`title`, `description`, `date`, `time`, `location`, `category`, `price`, `image`) VALUES ('Título Exemplo #2', 'Descrição exemplo.', '2024-01-01', '18:30:00', 'Sete Lagoas, MG', 'bares', 300.00, 'img_path/img_02.png');
-
-INSERT INTO `events` (`title`, `description`, `date`, `time`, `location`, `category`, `price`, `image`) VALUES ('Título Exemplo #3', 'Descrição exemplo.', '2023-12-23', '20:45:00', 'Belo Horizonte, MG', 'shows', 80.00, 'img_path/img_03.png');
-
-INSERT INTO `registrations` (`user_id`, `event_id`, `payment_status`) VALUES (1, 1, TRUE);
-
-INSERT INTO `registrations` (`user_id`, `event_id`, `payment_status`) VALUES (1, 2, TRUE);
-
-INSERT INTO `registrations` (`user_id`, `event_id`, `payment_status`) VALUES (1, 3, TRUE);
 --
